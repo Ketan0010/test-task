12471313ea32c5e19faff7c42aaea6d4d6a36b44
Ketan0010 <ketan.da143@gmail.com>
2025-07-29 02:09:28 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-bucket-tfstate1"
-    key            = "envs/prod/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "terraform-tfstate-table"
-    encrypt        = true
-  }
-}
