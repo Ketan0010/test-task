a2665f3708dd513a1edcf4c4e3ace6c4b1b6585c
Ketan0010 <ketan.da143@gmail.com>
2025-07-29 02:18:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,13 +11,13 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "public_subnets_cidrs" {
+variable "public_subnet_cidrs" {
   description = "List of public subnet CIDRs"
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "private_subnets_cidrs" {
+variable "private_subnet_cidrs" {
   description = "List of private subnet CIDRs"
   type        = list(string)
   default     = ["10.0.3.0/24", "10.0.4.0/24"]
